958a9a2c00e7719f31760f3a51b54889db189777
MohammadAmin <aminsec10@gmail.com>
2024-11-07 04:14:18 -0600

    jwt.js middleware refactored and commented

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,30 +1,33 @@
 const jwt = require('jsonwebtoken');
 const { dead_sessionsTB } = require("../database");
-
+//A middleware to validate JWT token
 async function validateJWT(req, resp, next){
-    if(!req.cookies.token){
-      resp.redirect("/login");
-      return
+  //Redirecting to /login if token is not found
+  if(!req.cookies.token){
+    resp.redirect("/login");
+    return
+  }
+  //Getting token from cookies
+  const token = req.cookies.token;
+  //Checking if the token has expired
+  const dead_sessions = await dead_sessionsTB.findOne({
+    where: {
+      session: token
     }
-    const token = req.cookies.token;
-
-    const dead_sessions = await dead_sessionsTB.findOne({
-      where: {
-        session: token
-      }
-    });
-
-    if(dead_sessions !== null){
-      resp.redirect("/login");
-      return
-    }
-
-    try {
-      jwt.verify(token, process.env.JWT_SECRET);
-      next();
-    } catch (error) {
-      resp.redirect("/login");
-    }
+  });
+  //Redirecting /login if DB query was not null. Null means token has not expired
+  if(dead_sessions !== null){
+    resp.redirect("/login");
+    return
+  }
+  //Verifing token in try-catch. If token was not valid, it will go through an error and we handle it with catch
+  try {
+    const userInfo = jwt.verify(token, process.env.JWT_SECRET);
+    req.userInfo = userInfo; // Assigning user's info to req as an object
+    next();
+  } catch (error) {
+    resp.redirect("/login");
+  }
 }
-
+//Exporting module
 module.exports = validateJWT;
