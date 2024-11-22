483cde68b4b99cb130bbda92bbe8f9c26a6c5fbc
MohammadAmin <aminsec10@gmail.com>
2024-11-22 00:44:22 -0600

    messages.js refactored

--- Routes/user/messages.js
+++ Routes/user/messages.js
@@ -2,28 +2,28 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { sendResponse } = require("../../utils/opt");
-const { validateUserInputAsNumber } = require("../../utils/validate");
+const { validateUserInputAsNumber, isUndefined } = require("../../utils/validate");
 const { messagesTB } =  require("../../database");
 
 router.get("/:contact", async (req, resp) => {
+    const { userInfo } = req;
     const { contact } = req.params;
     var { limit } = req.query;
     var { offset } = req.query;
-
+    
     //Validating limit and offset value
-    if((!limit || !offset) || (limit > 1000 || offset > 1000)){
+    if((await isUndefined(resp, limit, offset)) || (limit > 1000 || offset > 1000)){
         const message = {state: "failed", message: "Invalid limit or offset value"};
         sendResponse(message, resp);
         return
     }
 
-    if(!validateUserInputAsNumber(limit) || !validateUserInputAsNumber(offset)){
+    if(! await validateUserInputAsNumber(limit, offset)){
         const message = {state: "failed", message: "Invalid limit or offset value"};
         sendResponse(message, resp);
         return
     }
-
-    const userInfo = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    
     //Converting
     limit = Number(limit);
     offset = Number(offset);
@@ -49,22 +49,12 @@ router.get("/:contact", async (req, resp) => {
     });
 
     if(sentMessages && receivedMessages){
-        const message = {
-            state: "success",
-            messages: {
-                sents: sentMessages,
-                receiveds: receivedMessages
-            }
-
-        };
-
+        const message = {state: "success", messages: {sents: sentMessages,receiveds: receivedMessages}};
         sendResponse(message, resp);
     }else{
         const message = {state: "failed", message: "User not found"};
         sendResponse(message, resp);
     }
-
-
 });
 
 module.exports = router;
